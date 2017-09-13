214e0dfb9137c391bbb8c0fc3409fa36ca7542a6
meghna-saxena <meghna.s1906@gmail.com>
2017-09-13 12:25:54 +0530

    removed comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,3 @@
-// module.exports = {
-//   entry: [
-//     './src/index.js'
-//   ],
-//   output: {
-//     path: __dirname,
-//     publicPath: '/',
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     loaders: [{
-//       exclude: /node_modules/,
-//       loader: 'babel',
-//       query: {
-//         presets: ['react', 'es2015']
-//       }
-//     }]
-//   },
-//   resolve: {
-//     extensions: ['', '.js', '.jsx']
-//   },
-//   devServer: {
-//     historyApiFallback: true,
-//     contentBase: './'
-//   }
-// };
-
-
 const path = require('path');
  module.exports = {
    entry: './index.js',
