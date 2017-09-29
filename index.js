2406315a7fe6101c85f77e1722e41ace91dbe80c
meghna-saxena <meghna.s1906@gmail.com>
2017-09-29 10:51:07 +0530

    used create-react-app boilerplate

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/components/app';
-
-ReactDOM.render(<App />, document.querySelector('.container'));
--- test/components/components.spec.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('the test environment', () => {
-  it('works, hopefully', () => {
-    expect(true).to.be.true;
-  });
-});
